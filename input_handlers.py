212862b069625548be0bc86af3a228b74ad810f3
Adam Tokarsky <36281850+adtok@users.noreply.github.com>
2019-07-25 11:44:50 -0700

    Implemented items
    Items are randomly spawned, can be picked up, used, or dropped.

--- input_handlers.py
+++ input_handlers.py
@@ -1,6 +1,16 @@
 import tcod as libtcod
 
-def handle_keys(key):
+from game_states import GameStates 
+
+def handle_keys(key, game_state):
+    if game_state == GameStates.PLAYERS_TURN:
+        return handle_player_turn_keys(key)
+    elif game_state == GameStates.PLAYER_DEAD:
+        return handle_player_dead_keys(key)
+    elif game_State in (GameStates.SHOW_INVENTORY, GameStates.DROP_INVENTORY):
+        return handle_inventory_keys(key)
+
+def handle_player_turn_keys(key):
     key_char = chr(key.c)
     
     if key.vk == libtcod.KEY_UP or key_char == 'k':
@@ -20,6 +30,15 @@ def handle_keys(key):
     elif key_char == 'n':
         return {'move': (1, 1)}
 
+    if key_char == 'g':
+        return {'pickup': True}
+
+    if key_char == 'i':
+        return {'show_inventory': True}
+
+    if key_char == 'd':
+        return {'drop_inventory': True}
+
     if key.vk == libtcod.KEY_ENTER and key.lalt:
         #allow the user to toggle fullscreen
         return {'fullscreen': True}
@@ -27,4 +46,32 @@ def handle_keys(key):
     elif key.vk == libtcod.KEY_ESCAPE:
         return {'exit': True}
     
+    return {}
+
+def handle_player_dead_keys(key):
+    key_char = chr(key.c)
+
+    if key_char == 'i':
+        return {'show_inventory': True}
+    
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        # Alt+Enter - toggle fullscreen
+        return {'fullscreen': True}
+    elif key.vk == libtcod.KEY_ESCAPE:
+        # Exit the menu
+        return {'exit': True}
+    
+    return {}
+
+def handle_inventory_keys(key):
+    index = key.c - ord('a')
+
+    if index >= 0:
+        return {'inventory_index': index}
+    
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        return {'fullscreen': True}
+    elif key.vk == libtcod.KEY_ESCAPE:
+        return {'exit': True}
+
     return {}
--- render_functions.py
+++ render_functions.py
@@ -2,6 +2,10 @@ import tcod as libtcod
 
 from enum import Enum 
 
+from game_states import GameStates
+
+from menus import inventory_menu
+
 
 class RenderOrder(Enum):
     CORPSE = 1
@@ -33,7 +37,7 @@ def render_bar(panel, x, y, total_width, name, value, maximum, bar_color, back_c
                              f"{name}: {value}/{maximum}")
 
 def render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, message_log, screen_width, screen_height, 
-               bar_width, panel_height, panel_y, mouse, colors):
+               bar_width, panel_height, panel_y, mouse, colors, game_state):
     #  Draw all the tiles in game_map
     if fov_recompute:
         for y in range(game_map.height):
@@ -79,6 +83,12 @@ def render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, m
     libtcod.console_print_ex(panel, 1, 0, libtcod.BKGND_NONE, libtcod.LEFT, get_names_under_mouse(mouse, entities, fov_map))
 
     libtcod.console_blit(panel, 0, 0, screen_width, panel_height, 0, 0, panel_y)
+
+    if game_state in (GameStates.SHOW_INVENTORY, GameStates.DROP_INVENTORY):
+        action = 'use' if game_state == GameSates.SHOW_INVENTORY else 'drop'
+        inventory_title = f'Press the key next to an item to {action} it'
+        
+        inventory_menu(con, inventory_title, player.inventory, 50, screen_width, screen_height)
 
 def clear_all(con, entities):
     for entity in entities:
