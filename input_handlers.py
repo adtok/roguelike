9ae4708ea28876d5b1d4f6da59f4c1e4806257c2
Adam Tokarsky <36281850+adtok@users.noreply.github.com>
2019-07-26 16:12:10 -0700

    Added floors and player leveling up

--- input_handlers.py
+++ input_handlers.py
@@ -11,6 +11,12 @@ def handle_keys(key, game_state):
         return handle_targeting_keys(key)
     elif game_state in (GameStates.SHOW_INVENTORY, GameStates.DROP_INVENTORY):
         return handle_inventory_keys(key)
+    elif game_state == GameStates.LEVEL_UP:
+        return handle_level_up_menu(key)
+    elif game_state == GameStates.CHARACTER_SCREEN:
+        return handle_character_screen(key)
+
+    return {}
 
 def handle_player_turn_keys(key):
     key_char = chr(key.c)
@@ -31,6 +37,8 @@ def handle_player_turn_keys(key):
         return {'move': (-1, 1)}
     elif key_char == 'n':
         return {'move': (1, 1)}
+    elif key_char == 'z':
+        return {'wait': True}
 
     if key_char == 'g':
         return {'pickup': True}
@@ -40,6 +48,12 @@ def handle_player_turn_keys(key):
 
     if key_char == 'd':
         return {'drop_inventory': True}
+
+    if key.vk == libtcod.KEY_ENTER:
+        return {'take_stairs': True}
+
+    if key_char == 'c':
+        return {'show_character_screen': True}
 
     if key.vk == libtcod.KEY_ENTER and key.lalt:
         #allow the user to toggle fullscreen
@@ -93,6 +107,25 @@ def handle_main_menu(key):
     elif key_char == 'c' or key == libtcod.KEY_ESCAPE:
         return {'exit': True}
     
+    return {}
+
+def handle_level_up_menu(key):
+    if key:
+        key_char = chr(key.c)
+
+        if key_char == 'a':
+            return {'level_up': 'hp'}
+        elif key_char == 'b':
+            return {'level_up': 'str'}
+        elif key_char == 'c':
+            return {'level_up': 'def'}
+    
+    return {}
+
+def handle_character_screen(key):
+    if key.vk == libtcod.KEY_ESCAPE:
+        return {'exit': True}
+
     return {}
 
 def handle_mouse(mouse):
